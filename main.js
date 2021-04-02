287f7c38597cb02114d384b2dbba3029a2bae569
bekzod <bekzod@gmail.com>
2021-04-02 23:58:01 +0500

    Full

---
+++ main.js
@@ -0,0 +1,59 @@
+// birinchi boshlandi
+// var age = prompt('yoshingizni kiriting');
+
+// if (age > 0 && age <= 18){
+//     alert('Yoshakansan o`qishing kerak ')
+// }
+// else if (age > 18 && age <= 50 ){
+//     alert('Ishlashingiz kerak')
+// }
+// else if (age > 50 && age <= 59 ){
+//     alert('Yaqinda pensiyaga chiqasiz')
+// }
+// else if (age > 59 && age <= 150 ){
+//     alert('Pensionerga oxshaysiz agar tirik bolsangiz  ')
+// }
+// else {
+//     alert('Hozir siz qayerdasiz biz sizni topib Nobel mukofoti berib , Ginnes rekordlar kitobiga kiritamiz sekin sirini etasiz kop yashashshi ')
+// }
+
+// birinchi tugadi
+// ikkinchin boshlandi
+
+// var num1 = prompt('Birinchi sonni kiriting');
+// var num2 = prompt('ikkinchi sonni kiriting');
+// var num3 = prompt('Uchinchi sonni kiriting');
+
+
+// if (num1 < num2 && num1 > num3){
+//     alert('O`rtacha qiymat ' + num1)
+// }
+// else if (num1 < num3 && num1 > num2){
+//     alert('O`rtacha qiymat ' + num1)
+// }
+// else if (num2 < num1 && num2 > num3){
+//     alert('O`rtacha qiymat ' + num2)
+// }
+// else if (num2 < num3 && num2 > num1){
+//     alert('O`rtacha qiymat ' + num2)
+// }
+// else if (num3 < num2 && num3 > num1){
+//     alert('O`rtacha qiymat ' + num3)
+// }
+// else if (num3 < num1 && num3 > num2){
+//     alert('O`rtacha qiymat ' + num3)
+// }
+
+
+
+// ikkinchin tugadi
+// uchinchi boshlandi 
+
+//  var ismKiritish = prompt('Ismingizni kiriting');
+//  var ismBoss = 'usmon';
+//  if (ismKiritish == ismBoss){
+//      alert('KELDIMI OG`RI')
+//  }
+//  else {
+//      alert('UYINI YOQVORISHVOTTI ')
+//  }
